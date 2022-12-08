fe9741ccac09b995183bc7aa036a3a0dfb784717
Jachian <author.tle@gmail.com>
2022-12-08 17:17:32 -0500

    complete network graph

--- js/emailContactDistribution.js
+++ js/emailContactDistribution.js
@@ -3,23 +3,69 @@ d3.queue()
   .defer(d3.json, "data/employee-email-contact-data.json")
   .await(ready);
 
-function ready(error, typeTotalData, data) {
+// Global
+let globalData1;
+let globalData2;
+
+function ready(error, data1, data2) {
   if (error) throw error;
+
+  globalData1 = data1;
+  globalData2 = data2;
+
+  drawNetworkChart(null);
+}
+
+function drawNetworkChart(nodeId){
+  const typeTotalData = globalData1;
+  const emailNetworkData = globalData2;
+  if (nodeId) {
+    let existingNodes = [];
+    let links = [];
+    let nodes = [];
+    for(let i = 0; i < emailNetworkData.links.length; i ++) {
+      if (emailNetworkData.links[i].source.id == nodeId || emailNetworkData.links[i].target.id == nodeId) {
+        existingNodes.push(emailNetworkData.links[i].source.id);
+        existingNodes.push(emailNetworkData.links[i].target.id);
+        links.push(emailNetworkData.links[i]);
+      }
+    }
+    for(let i = 0; i < emailNetworkData.nodes.length; i ++) {
+      if (emailNetworkData.nodes[i].id == nodeId || existingNodes.includes(emailNetworkData.nodes[i].id)) {
+        nodes.push(emailNetworkData.nodes[i]);
+      }
+    }
+    emailNetworkData.links = links;
+    emailNetworkData.nodes = nodes;
+  }
+  const links = emailNetworkData.links;
+  const nodes = emailNetworkData.nodes;
 
   const innerWidth = window.innerWidth;
   const margin = {top: 30, right: 30, bottom: 30, left: 30},
     padding = {top: 30, right: 30, bottom: 30, left: 30},
     width = innerWidth - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    height = 600 - margin.top - margin.bottom,
+    legendWidth = 250,
+    graph1Height = 600;
+  const legendMarkRadius = 12;
+  const circleDefaultRadius = 10;
+  const circleDefaultRadiusBase = 10;
+  const rowHeight = 25;
+  const rowPadding = 10;
+  const linkDistance = 150;
 
-  const groups = typeTotalData.map(x => x["INDEX"]);  
+  let groups = typeTotalData.map(x => x["INDEX"]);  
+  groups.push("-1");
+  let types = typeTotalData.map(x => x["TYPE"]);  
+  types.push("Undefined");
+
   const cScale = d3.scaleOrdinal().domain(groups).range(d3.schemeTableau10);
 
   // Create a tooltip
-  const tooltip = d3.select('#employeeEmailContactDistribution')
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "position-absolute bg-white border border-dark border-2 rounded-1 p-2");
+  const tooltip = d3
+    .select('#employeeEmailContactDistributionTooltip')
+    .style("opacity", 0);
     
   // Mouse functions
   const mouseLeave = function (d) {
@@ -30,20 +76,35 @@ function ready(error, typeTotalData, data) {
     tooltip.style("opacity", 1);
   }
 
-  let mouseMove = function(d) {
+  let mouseMove = function(event, d) {
     const array = d.name.split("@");
-    let str = array[0];
+    let str = array[0].split(".").join(" ")
     tooltip.html(str)
-      .style("left", (d3.event.x + 20) + "px")
-      .style("top", (d3.event.y + 500) + "px")
+      .style("left", (event.x + 20) + "px")
+      .style("top", (event.y + graph1Height) + "px")
       .style("color", "black")
       .style("z-index", 99)
   }
 
-  let simulation = d3.forceSimulation(data.nodes) 
-    .force("link", d3.forceLink().id(function(d) { return d.id; }).links(data.links))
-    .force("charge", d3.forceManyBody().strength(-600))     
-    .force("center", d3.forceCenter(width / 2, height / 2))
+  const forceLink = d3.forceLink()
+    .id(function(d) { 
+      return d.id; 
+    })
+    .links(links)
+    .distance(linkDistance);
+
+  let simulation = d3.forceSimulation(nodes) 
+    .force("link", forceLink)
+    .force("charge", d3.forceManyBody().strength(-width / 2))     
+    .force('x', d3.forceX().x(function(d) {
+      if (d.group > 0) {
+        return legendWidth + (width - legendWidth) / groups.length * (parseInt(d.group));
+      }
+      return width / groups.length;
+    }))
+    .force('y', d3.forceY().y(function(d) {
+      return height / 2;
+    }))
   ;
 
   // Build basic graph
@@ -56,50 +117,109 @@ function ready(error, typeTotalData, data) {
 
   // Initialize the links
   let link = svg
+    .append("g")
     .selectAll("line")
-    .data(data.links)
+    .data(links)
     .enter()
     .append("line")
     .attr("class", "link")
-    .style("stroke", function (d) {
-      if (d["number"] < 10) {
-        return "gray";
-      } else if (d["number"] < 20) {
-        return "blue";
-      } else if (d["number"] < 30) {
-        return "orange";
-      } else {
-        return "red";
-      }
-    })
+    .style("stroke", "gray");
+
+  if (nodeId == null) {
+    link.style("stroke-width", function (d) {
+      return d["number"] / 10;
+    });
+  } 
 
   // Initialize the nodes
-  let node = svg.selectAll(".node")
-    .data(data.nodes)
+  let node = svg
+    .append("g")
+    .selectAll(".node")
+    .data(nodes)
     .enter()
     .append("g")
     .attr("class", "node");
 
-  node.append("circle")
-    .attr("x", -8)
-    .attr("y", -8)
-    .attr("r", 16)
+  let circle = node.append("circle")
     .attr("fill", function(d) {
       return cScale(d.group);
     })
     .on("mouseover", mouseOver)
     .on("mousemove", mouseMove)
     .on("mouseleave", mouseLeave)
-    .call(drag(simulation));
+    .call(drag(simulation))
+    .on("click", function(event, d) {
+      updateNetworkGraph(d.id); 
+    });
+
+  if (nodeId) {
+    circle.attr("r", function(d) {
+      if (nodeId == d.id) {
+        return circleDefaultRadius;
+      } else {
+        for(let i = 0; i < links.length; i ++) {
+          if (d.id == links[i].source.id || d.id == links[i].target.id) {
+            return parseInt(links[i].number + circleDefaultRadiusBase);
+          }
+        }
+      }
+    });
+  } else {
+    circle.attr("r", circleDefaultRadius);
+  }
 
   simulation.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+     link
+      .attr("x1", function(d) { 
+        let x1 = legendWidth + d.source.x < width ? legendWidth + d.source.x : width;
+        return x1; 
+      })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { 
+        let x2 = legendWidth + d.target.x < width ? legendWidth + d.target.x : width;
+        return x2; 
+      })
+      .attr("y2", function(d) { return d.target.y; });
+  
+    node
+      .attr("transform", function(d) { 
+        let x = legendWidth + d.x < width ? legendWidth + d.x : width;
+        return "translate(" + x + "," + d.y + ")"; 
+      });
   });
+
+  // Legend
+  const legend = svg
+    .append("g")
+    .selectAll(".legend") 
+    .data(groups)
+    .enter()
+    .append("g")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+    .attr("class", "legend");   
+
+  legend.append("circle")
+    .attr("cy", function(d, i) {
+      return height / groups.length + (rowHeight + rowPadding) * i;
+    })
+    .attr("r", legendMarkRadius)
+    .attr("fill", function(group) {
+      return cScale(group);
+    });
+
+  // Add the text
+  legend.append("text") 
+    .text(function(group){
+      if (group == -1) {
+        return types[types.length - 1];
+      }
+      return types[group];
+    })
+    .attr("class", "fs-6")
+    .attr("y", function (d, i) {
+      return height / groups.length + (rowHeight + rowPadding) * i;
+    })
+    .attr("x", margin.left);  
 
   function drag(simulation) {    
     function dragstarted(event) {
